dab70a0beb3f66e7a2e851601a53974779e3e2ec
Пользователь <suhrob.coder0604@gmail.com>
2023-08-15 17:43:51 +0300

    finish

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,7 +1,6 @@
 import Swiper from "https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.mjs";
 var swiper = new Swiper(".swiper", {
   slidesPerView: 1,
-
   spaceBetween: 20,
   loop: true,
 
@@ -20,6 +19,20 @@ var swiper = new Swiper(".swiper", {
       slidesPerView: 2,
       centeredSlides: true,
       spaceBetween: 50,
+    },
+  },
+});
+
+const mySwiper = new Swiper(".swiper-cards", {
+  slidesPerView: 1,
+  pagination: {
+    el: ".swiper-pagination-2",
+    clickable: true,
+  },
+  breakpoints: {
+    420: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
     },
   },
 });
